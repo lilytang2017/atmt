c849b95221793d24b95b52a3fec9a520d5e02b6d
Li Tang <li.tang@uzh.ch>
2020-11-29 16:43:30 +0100

    task 1: experiment with beam search

--- getBLEU.sh
+++ getBLEU.sh
@@ -1,3 +1,3 @@
-python translate.py
-./postprocess.sh model_translations.txt model_translations.out en
-cat model_translations.out | sacrebleu baseline/raw_data/test.en
+python translate_beam.py --beam-size 11
+./postprocess_asg4.sh model_translations.txt model_translations.out en
+cat model_translations.out | sacrebleu data_asg4/raw_data/test.en
